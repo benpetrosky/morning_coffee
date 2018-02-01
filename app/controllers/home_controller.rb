889dbb515d81c1c48df25c2da2a3b64c7c65a22b
Ben Petrosky <benpetrosky@hotmail.com>
2018-01-31 16:55:18 -0800

    allow users to see the top stories of 1-5 of their interests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,43 @@
 class HomeController < ApplicationController
   def index
     @user = current_user
-    @feedItems = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).first.interest_name)
+
+    if @user.interests.where(searchable: true).any?
+      @numberOfFeeds = @user.interests.where(searchable: true).count
+      if @numberOfFeeds === 5
+        @feed5 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).fifth.interest_name)
+        @feed4 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).fourth.interest_name)
+        @feed3 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).third.interest_name)
+        @feed2 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).second.interest_name)
+        @feed1 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).first.interest_name)
+      elsif @numberOfFeeds === 4
+        @feed4 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).fourth.interest_name)
+        @feed3 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).third.interest_name)
+        @feed2 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).second.interest_name)
+        @feed1 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).first.interest_name)
+      elsif @numberOfFeeds === 3
+        @feed3 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).third.interest_name)
+        @feed2 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).second.interest_name)
+        @feed1 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).first.interest_name)
+      elsif @numberOfFeeds === 2
+        @feed2 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).second.interest_name)
+        @feed1 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).first.interest_name)
+      else
+        @feed1 = GoogleCustomSearchApi.search(@user.interests.where(searchable: true).first.interest_name)
+      render :index
+    end
+    else
     render :index
   end
 end
+end
+
+
+# def update
+#   @product = Product.find(params[:id])
+#   if @product.update(product_params)
+#     redirect_to products_path
+#   else
+#     render :edit
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'news/index'
+  # get 'news/index'
 
    root :to => 'home#index'
 
